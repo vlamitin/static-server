1118ae5d7d1eb7ebae75a7d7fc963492cf9ae715
Vladimir Mitin <vmitin.dev@gmail.com>
2019-06-22 12:19:00 +0300

    added https support

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const express = require('express');
+const http = require ('http');
+const https = require('https');
+const fs = require('fs');
 
 const server = express();
 const defaultBuildPath = path.join(__dirname, './build');
@@ -9,17 +12,27 @@ const buildPath = path.resolve(args.build) || defaultBuildPath
 
 server.use(express.static(buildPath));
 
-server.all('*', function(req, res, next) {
+server.all('*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
     next();
 });
 
-server.get('*', function (req, res) {
+server.get('*', (req, res) => {
     res.sendFile(buildPath + '/index.html');
 });
 
-server.listen(port, () => {
-    console.log('Static Server is running on port: ' + port);
-});
+http.createServer(server).listen(port, () => console.log('Static Server is running http on port: ' + port));
+
+if (args.publicCert && args.privateCert) {
+    const publicCertPath = path.resolve(args.publicCert);
+    const privateCertPath = path.resolve(args.privateCert);
+
+    https
+	.createServer({
+	    cert: fs.readFileSync(publicCertPath, 'utf8'),
+            key: fs.readFileSync(privateCertPath, 'utf8'),
+        }, server)
+	.listen(443, () => console.log('Static Server is running https on port: 443'));
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-node index.js --port=3002 --build="path/to/build/dir"
+node index.js --port=3002 --build="path/to/build/dir" [--publicCert="path/to/pub.pem" --privateCert="path/to/priv.pem"]
